3d4519fe544d3f9799736b95cdb3a9b162fa37dd
HunterOBrien <obrienh2@middleton.school.nz>
2022-05-10 14:28:15 +1200

    Create 04_Game_Mechanics_v1.py

---
+++ 04_Game_Mechanics_v1.py
@@ -0,0 +1,25 @@
+"""  game mechanics v1
+Creating functions that hold information about the quizzes
+
+"""
+
+
+def months_quiz():
+    months_questions = [""]
+
+
+def days_quiz():
+    days_questions = [""]
+
+
+def numbers_quiz():
+    numbers_questions = [("Toru means one in Maori", False)
+                         ("Whitu is number seven", True)
+                         ("Tekau is ten", True)
+                         ("Tahi is number three", False)
+                         ("Rima is five", True)
+                         ("Kore is nine", False)
+                         ("Ono is six", True)
+                         ("Waru is two", False)
+                         ("Rua is eight", False)
+                         ("Iwa is nine", True)]
