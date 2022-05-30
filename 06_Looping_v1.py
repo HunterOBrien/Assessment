12a3f3ff9a92de76a13c58c9d50461a44bc8b2e9
HunterOBrien <obrienh2@middleton.school.nz>
2022-05-30 17:14:33 +1200

    update

--- 06_Looping_v1.py
+++ 06_Looping_v1.py
@@ -3,6 +3,14 @@ This turns the quizzes into a re-playable loop
 """
 
 
+# Function to format text object
+def formatter(symbol, text):
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f" {top_bottom}\n {formatted_text}\n {top_bottom}"
+
+
 # works out what difficulty the user wants
 def difficulty_selection(question_text):
     while True:
@@ -12,11 +20,11 @@ def difficulty_selection(question_text):
 
         # If they say 1, output: program continues on easy
         if answer == "1" or answer == "one":
-            game_mechanics_easy()
+            print()
 
         # If they say 2, output: program continues on medium
         elif answer == "2" or answer == "two":
-            game_mechanics_medium()
+            print()
 
         # If they say 3, output: program continues on hard
         elif answer == "3" or answer == "three":
@@ -27,100 +35,9 @@ def difficulty_selection(question_text):
             print("Please answer 1, 2, or 3")
 
 
-# Easy quiz function
-def game_mechanics_easy():
-    # Stores quiz values
-    questions_nums = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"]
-    question_values = ["f", "t", "t", "f", "t", "f", "t", "f", "f", "t"]
-    questions_text = [("Toru means one in Maori"),
-                      "Whitu is number seven",
-                      "Tekau is ten",
-                      "Tahi is number three",
-                      "Rima is five",
-                      "Kore is nine",
-                      "Ono is six",
-                      "Waru is two",
-                      "Rua is eight",
-                      "Iwa is nine"]
-    # Stores the previous answer and checks it against the question_values list
-    answer_storer = ""
-    # Keeps track of your score and the amount of questions
-    score = int(0)
-    total_questions = int(0)
-    # Breaks the quiz loop
-    quiz_loop_breaker = ""
-
-    # For loop that does the quiz with the given variables
-    for questions_printer in range(len(questions_nums)):
-        print(questions_nums[questions_printer])
-        print(questions_text[questions_printer])
-        answer_storer = input("enter answer here: ").lower()
-        quiz_loop_breaker = "x"
-        while quiz_loop_breaker == "x":
-            if answer_storer == "f" or answer_storer == "t":
-                if answer_storer == question_values[questions_printer]:
-                    print("You got it right, Great Job!")
-                    score += 1
-                    total_questions += 1
-                    quiz_loop_breaker = "y"
-
-                elif answer_storer != question_values[questions_printer]:
-                    print("You got it wrong, Unlucky!")
-                    total_questions += 1
-                    quiz_loop_breaker = "y"
-
-            else:
-                print("please enter a valid input, (t or f)")
-                quiz_loop_breaker = "y"
-    # Prints the users final score
-    print(f"You got {score} out of {total_questions}")
-
-
-# Medium quiz function
-# Check game_mechanics_easy for comments in the function
-def game_mechanics_medium():
-    questions_nums = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"]
-    question_values = ["t", "t", "f", "f", "t", "t", "f", "f"]
-    questions_text = ["There are two different Maori names for each day of the week",
-                      "Rahina is one name for Monday",
-                      "Ratu is a name for Wednesday",
-                      "Raapa is a name for Monday",
-                      "Rapare is the name for Thursday",
-                      "Ramere is the name for Friday",
-                      "Rahoroi is the name for Sunday",
-                      "Ratapu is the name for Saturday", ]
-
-    answer_storer = ""
-    score = int(0)
-    total_questions = int(0)
-    quiz_loop_breaker = ""
-
-    for questions_printer in range(len(questions_nums)):
-        print(questions_nums[questions_printer])
-        print(questions_text[questions_printer])
-        answer_storer = input("enter answer here: ").lower()
-        quiz_loop_breaker = "x"
-        while quiz_loop_breaker == "x":
-            if answer_storer == "f" or answer_storer == "t":
-                if answer_storer == question_values[questions_printer]:
-                    print("You got it right!")
-                    score += 1
-                    total_questions += 1
-                    quiz_loop_breaker = "y"
-
-                elif answer_storer != question_values[questions_printer]:
-                    print("You got it wrong!")
-                    total_questions += 1
-                    quiz_loop_breaker = "y"
-
-            else:
-                print("please enter a valid input, (t or f)")
-                quiz_loop_breaker = "y"
-    print(f"You got {score} out of {total_questions}")
-
-
 # Hard quiz function
 # Check game_mechanics_easy for comments in the function
+# Testing function for looping
 def game_mechanics_hard():
     questions_nums = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"]
     question_values = ["t", "f", "f", "f", "t", "t", "t", "f", "f", "t", "t", "f"]
@@ -162,25 +79,31 @@ def game_mechanics_hard():
             else:
                 print("please enter a valid input, (t or f)")
                 quiz_loop_breaker = "y"
+    print(formatter("!", "Well Played"))
     print(f"You got {score} out of {total_questions}")
     looping()
 
+
+# Function to make the game a playable loop
 def looping():
-    print("Thanks for playing, would you like to play again?")
-    play_again = input("If you want to play again enter 'Y'\n"
-                       "If you do not want to play again enter 'N'\n "
+    finish_game = ""
+    print("Do you want to do a Te Reo quiz?")
+    play_again = input("If you want to play enter 'Y'\n"
+                       "If you do not want to play enter 'N'\n "
                        ":  ").upper()
 
-    while True:
+    while finish_game != "x":
         if play_again == "Y" or play_again == "YES":
-            difficulty_selection()
+            difficulty_selection("What difficulty do you want to play?\n"
+                                 "1 for easy, 2 for medium, 3 for hard: ")
 
         elif play_again == "N" or play_again == "NO":
-            difficulty_selection()
+            print(formatter("*", "Thanks for playing... Goodbye"))
+            finish_game = "x"
         else:
-            print("Please enter a valid input")
+            print("Please enter a valid input (Y or N)")
+            looping()
 
 
-# Gets the difficulty the user wants
-difficulty_selection("What difficulty do you want to play?\n"
-                     "1 for easy, 2 for medium, 3 for hard: ")
+# Main
+looping()
